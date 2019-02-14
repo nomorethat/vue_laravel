5a7bf91a9af90647aee85d050d68ad4cb22d0ef1
Дмитрий <dm_lis@mail.ru>
2019-02-14 15:57:41 +0300

    Начальная настройка передачи маршрутов в JS скрипты. Сформирован файл routes.json. Формируется при помощи пользовательской команды php artisan route:json

---
+++ app/Console/Commands/GenerateRoutesJSON.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Routing\Router;
+use File;
+
+class GenerateRoutesJSON extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'route:json';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+    protected $router;
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(Router $router)
+    {
+        parent::__construct();
+        $this->router = $router;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $routes = [];
+        foreach ($this->router->getRoutes() as $route){
+            $routes[$route -> getName()] = $route -> uri();
+        }
+        File::put('resources/assets/js/routes.json', json_encode($routes, JSON_PRETTY_PRINT));
+    }
+}
